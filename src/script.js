6183f75e689fd10fba35714ecdf4616d40bdeca7
Higor Festa <higornatan73@gmail.com>
2024-02-19 17:45:23 -0300

    Colocando o primeiro projeto entregável(calculadora)

--- src/script.js
+++ src/script.js
@@ -1,7 +1,38 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let resultado = 0
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numero1, numero2, operador){
+  let num1 = Number(numero1)
+  let num2 = Number(numero2)
+
+  switch(operador){
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      resultado =+ num1
+      for(contar = 1; contar < num2; contar++){
+        resultado = resultado * num1
+      }
+  }
+  if(resultado == undefined || resultado > 1000000){
+    return "Error"
+  }else{
+    return resultado
+  }
+}
+
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
